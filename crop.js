047bda44c136fbc591d470033b8c2de1c0a639f0
AngReload <allid@yandex.ru>
2017-03-16 16:37:48 +0500

    moved methods

--- crop.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-function crop(x, y, width, height) {
-	x = x || 0;
-	y = y || 0;
-	let getter = (v, c, w, h) => this.get(c, w + x, h + y);
-	return new this.constructor(width, height, this.components).map(getter);
-}
-module.exports = crop;
